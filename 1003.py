fa644932a5cac596d4562633e73fb004b3ac2419
ESENS <ghdldid12@gmail.com>
2018-03-19 13:04:44 +0900

    #1003문제 DP풀이 완료

--- 1003.py
+++ 1003.py
@@ -2,17 +2,6 @@
 백준 알고리즘 1003번 문제 풀이
 작성자 ESENS
 작성일 180319
-int fibonacci(int n) {
-    if (n == 0) {
-        printf("0");
-        return 0;
-    } else if (n == 1) {
-        printf("1");
-        return 1;
-    } else {
-        return fibonacci(n‐1) + fibonacci(n‐2);
-    }
-}
 
 첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 다음과 같이 구성되어있다.
 
@@ -20,23 +9,16 @@ int fibonacci(int n) {
 
 각 테스트 케이스마다 0이 출력되는 횟수와 1이 출력되는 횟수를 공백으로 구분해서 출력한다.
 
-입력
-3
-0
-1
-3
-
-출력
-1 0
-0 1
-1 2
 
 '''
+
+#=============================================여기서부터 테스트코드!
 #dp = dict()
 
 dp = [-1]*1000001
 dp[0] = 0
 dp[1] = 1
+dp[2] = 1
 
 # fib(5)이면 1 1 2 3 5
 # fib(10)이면 1 1 2 3 5 8 13 21 34 55
@@ -46,14 +28,9 @@ cnt_one = 0
 cnt = [-1]*2
 
 def fibonacci(n):
-    global cnt_zero
-    global cnt_one
 
     if dp[n] != -1:
-        if dp[n] == 0 : cnt_zero += 1
-        elif dp[n] == 1 : cnt_one += 1
         return dp[n]
-
     else:
         dp[n] = fibonacci(n - 2) + fibonacci(n - 1)
         return dp[n]
@@ -64,26 +41,48 @@ def basic_fibonacci(n):
 
     if n == 0:
         cnt_zero += 1
-        print("n is 0")
+        #print("n is 0")
+        return 1
     elif n == 1:
         cnt_one += 1
-        print("n is 1")
+        #print("n is 1")
+        return 1
     else:
         return basic_fibonacci(n-2) + basic_fibonacci(n-1)
+#0과 1은 fib(n-2)와 fib(n-1)만큼 호출된다..!
+# fib(10)이면 1 1 2 3 5 8 13 21 34 55
 
-print(basic_fibonacci(6))
+print(basic_fibonacci(2))
 print(str(cnt_zero) +" "+str(cnt_one))
-'''
+#=============================================여기까지 테스트 코드..피보나치 암산이 안돼서..
+
+# 실제 정답 코드
+# 0과 1의 개수를 세야 하는데 DP로 풀어야 초과가 안난다
+# 문제 초과 시간이 조금 빡빡해서 그런지 한번 푼 피보나치 데이터를 각 입력값 마다 초기화 하면 시간초과가 남.. 걍 재활용 하면 통과
+# N의 fib(0), fib(1)번 호출 횟수는 fib(n-1) , fib(n)번씩이다!
+dp = [-1] * 100001
+dp[0] = 0
+dp[1] = 1
+
+
+def fibonacci(n):
+    if dp[n] != -1:
+        return dp[n]
+    else:
+        dp[n] = fibonacci(n - 2) + fibonacci(n - 1)
+        return dp[n]
+
+
 case = int(input())
 
 for i in range(0, case):
     num = int(input())
     fibonacci(num)
-    print(str(cnt_zero) + " " + str(cnt_one))
-    cnt_one = 0
-    cnt_zero = 0
-
-print(dp[:10])
-'''
+    if num == 1:
+        print("0 1")
+    elif num == 0:
+        print("1 0")
+    else:
+        print(str(dp[num - 1]) + " " + str(dp[num]))
 
 
