48c18442d4ac9db64015f35b81ba56729e218d5b
ESENS <ghdldid12@gmail.com>
2018-03-27 11:33:50 +0900

    #11718 풀이 완료(문자열)

---
+++ 11718.py
@@ -0,0 +1,25 @@
+'''
+백준 알고리즘 11718 문제 풀이
+작성자 ESENS
+작성일 170327
+
+문자열 다루기
+입력받은 그대로 출력
+
+입력:
+Hello
+Baekjoon
+Online Judge
+
+------------------------------------------------------------------------------------
+
+출력:
+Hello
+Baekjoon
+Online Judge
+'''
+
+while True:
+    line = input()
+    if not line: break
+    print(line)
