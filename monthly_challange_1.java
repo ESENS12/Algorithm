b30eaeb633b5ce27b94f346a48a5f42fa5e16020
ESENS12 <ghdldid12@gmail.com>
2021-09-29 12:16:22 +0900

    Update TIL

---
+++ monthly_challange_1.java
@@ -0,0 +1,14 @@
+// 문제 출처 : https://programmers.co.kr/learn/courses/30/lessons/76501
+class Solution {
+   public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i=0; i< signs.length; i++){
+            if(!signs[i]){
+                answer = answer + (-(absolutes[i]));
+            }else{
+                answer += absolutes[i];
+            }
+        }
+        return answer;
+    }
+}
