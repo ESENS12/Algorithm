99d8df2ffd3aebbb5859825f86fd64b338dd7aef
ESENS <ghdldid12@gmail.com>
2021-10-01 13:24:21 +0900

    Update greedy_search_1.java

--- greedy_search_1.java
+++ greedy_search_1.java
@@ -14,6 +14,7 @@ class Solution {
             second = (answers[i] == guessAnswer(2,i+1)) ? second+1 : second;
             third = (answers[i] == guessAnswer(3,i+1)) ? third+1 : third;
         }
+
         //3명의 점수를 순회하면서 가장 큰값을 찾음
         int max_value = 0;
         if(max_value <= first){
@@ -28,12 +29,11 @@ class Solution {
             arr_answer.add(2);
         }
         if(max_value <= third){
-            if(second < third){
+            if(second < third && first < third){
                 arr_answer.remove(0);
             }
             arr_answer.add(3);
         }
-
         int[] answer = new int[arr_answer.size()];
 
         for(int i=0; i<arr_answer.size(); i++){
@@ -42,31 +42,44 @@ class Solution {
 
         return answer;
     }
-    
+    //수포자 3의 정답패턴
     private final int[] sequence = new int[]{3,3,1,1,2,2,4,4,5,5};
-    private final int[] sequence_second = new int[]{2,1,2,2,2,3,2,4,2,5};
+    //수포자 2의 정답패턴
+    private final int[] sequence_second = new int[]{2,1,2,3,2,4,2,5};
+    
     private int guessAnswer(int studentNum, int answerNum){
-        int guessNum = 1;
-        int real_num = answerNum;
-        if(answerNum > 10){
-            answerNum = answerNum % 10;
-            if(answerNum == 0){
-                answerNum = 10;
-            }
-        }
+        int guessNum = 1;           // 수포자가 찍은 정답
+        int real_num = answerNum;   // 디버깅용 실제 문제번호
+        
         switch (studentNum){
             case 1:{
-                if(answerNum > 5){
-                    answerNum = answerNum - 5;
+               if(answerNum > 5){   //수포자 1은 1,2,3,4,5 순으로만 반복하므로 간단하게 처리
+                    answerNum = answerNum % 5; 
+                    if(answerNum == 5){
+                        answerNum = 5;
+                    }
                 }
                 guessNum  = answerNum;
             }break;
 
             case 2:{
-                guessNum = sequence_second[answerNum-1];
+                if(answerNum > 8){  //수포자 2, 3은 미리입력해둔 패턴에 인덱스만 맞추면 되므로, (문제번호가 1~8을 반복하도록)
+                    answerNum = answerNum % 8;
+                    if(answerNum == 0){
+                        answerNum = 8;
+                    }
+                }
+               
+                guessNum = sequence_second[real_num-1];
             }break;
 
             case 3:{
+                if(answerNum > 10){
+                    answerNum = answerNum % 10;
+                    if(answerNum == 0){
+                        answerNum = 10;
+                    }
+                }
                 guessNum = sequence[answerNum-1];
             }break;
         }
