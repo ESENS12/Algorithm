695724b0c4fe99b21977c6a1cf04b018d00ce89a
ESENS12 <ghdldid12@gmail.com>
2022-03-31 10:08:01 +0900

    Update TIL

---
+++ iterator/two_pointer_1.java
@@ -0,0 +1,33 @@
+// 문제출처 https://leetcode.com/problems/two-sum/submissions/
+//음수처리 아직 안됨. 
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] results = {0,0};
+        
+        int lt = 0;
+        int rt = nums.length-1;
+        int temp = 0;
+        while(lt<=rt){
+            
+            temp = nums[lt] + nums[rt];
+            println("do while lt : " + lt + ", rt : " + rt + ", temp : " + temp);
+            if(temp == target){
+                results[0] = lt;
+                results[1] = rt;
+                break;
+            }
+            if(target < nums[rt]){
+                rt--;
+            }else if(target > nums[lt]){
+                lt++;
+            }
+        }
+        return results;
+    }
+    public void println(String str){
+        System.out.println(str);
+    }
+}
