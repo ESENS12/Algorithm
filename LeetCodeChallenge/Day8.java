611f6d20ef7de2a5c558f66937c962a5c49d179a
ESENS <ghdldid12@gmail.com>
2022-05-01 12:47:55 +0900

    Update TIL

---
+++ LeetCodeChallenge/Day8.java
@@ -0,0 +1,62 @@
+// BFS  (merge two treenode)
+
+class Solution {
+    
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1 == null) return root2;
+        if(root2 == null) return root1;
+        
+        TreeNode merge = new TreeNode(root1.val + root2.val);
+        merge.left = mergeTrees(root1.left, root2.left);
+        merge.right = mergeTrees(root1.right, root2.right);
+        
+        return merge;
+    }
+    
+}
+
+
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return null;
+        Node pre = root;
+        
+        while(pre.left != null){
+            Node curr = pre;
+            
+            while(curr != null){
+                curr.left.next = curr.right;
+                if(curr.next != null){
+                    curr.right.next = curr.next.left;
+                }
+                curr = curr.next;
+            }
+            pre = pre.left;
+        }
+        return root;
+    }
+}
