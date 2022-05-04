dba3ce11947186174439d191c704d74188e580ad
ESENS <ghdldid12@gmail.com>
2022-05-04 17:20:42 +0900

    Update greedy_two_point.java

--- greedy/greedy_two_point.java
+++ greedy/greedy_two_point.java
@@ -20,3 +20,57 @@ class Solution {
         return result;
     }
 }
+
+
+// 문제 
+// https://leetcode.com/problems/container-with-most-water/submissions/
+// O(n*2) 풀이
+
+class Solution {
+    public int maxArea(int[] height) {
+        if(height.length == 2){
+            return Math.min(height[0],height[1]);
+        }
+        int nResult = 1;
+    
+        for(int i=0; i<height.length; i++){
+            int ltDam = height[i];
+            for(int j=i+1; j<height.length; j++){
+                int rtDam = height[j];
+                int nTemp = Math.min(ltDam,rtDam);
+                nTemp = nTemp * (j-i);
+
+                if(nResult < nTemp){
+                    nResult = nTemp;
+                }
+            
+            }
+        }
+
+        return nResult;
+    }
+}
+
+
+---------------------------------------------------------------------------------
+// O(n) 풀이
+
+class Solution {
+    public int maxArea(int[] height) {
+        int lt = 0;
+        int rt = height.length - 1;
+	    int nMaxValue = 0;
+
+	    while (lt < rt) {
+		    nMaxValue = Math.max(nMaxValue, Math.min(height[lt], height[rt]) * (rt - lt));
+            
+            if (height[lt] < height[rt]){
+                lt++;
+            }else{
+                rt--;
+            }
+	    }
+
+	    return nMaxValue;
+    }
+}
